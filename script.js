f90431bbfe789276ad8364c224098bbb9127bf39
Eduardo <74874724+eduardotrj@users.noreply.github.com>
2025-06-27 16:42:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -98,27 +98,6 @@ document.addEventListener("scroll", (event) => {
     }
   })
 
-// Skill Page
 
-const tabs = document.querySelectorAll('[data-target]');
-const tabContent = document.querySelectorAll('[data-content]');
-
-tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.target);
-
-        tabContent.forEach(tabContents => {
-            tabContents.classList.remove('skills-active');
-        })
-
-        target.classList.add('skills-active');
-
-        tabs.forEach(tab => {
-            tab.classList.remove('skills-active');
-        })
-
-        tab.classList.add('skills-active');
-    })
-})
 
 
